9c5135a9511efbabbe2689cd9522326c09764593
Aaron Priestley <apriestley7@gmail.com>
2015-11-13 11:31:44 +1100

    added helloWorld.hs

---
+++ helloWorld.hs
@@ -0,0 +1,7 @@
+-- Hello World program in Haskell
+-- Aaron Priestley (apriestley7@gmail.com)
+-- 13th November 2015
+
+-- Compile with ghc with: ghc helloWorld.hs 
+main :: IO ()
+main = do putStrLn "Hello World :-)"
